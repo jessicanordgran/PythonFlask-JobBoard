268bdb6f65b612a1b540e14dd465eeaeee4a840d
jessicanordgran <jessicanordgran@mail.weber.edu>
2019-03-14 14:05:39 -0600

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -27,7 +27,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-    cennection = getattr(g, '_connection', None)
+    connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
